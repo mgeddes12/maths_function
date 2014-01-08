64e1c4e3858f9ef8b3e3af872fe4fd8fe3933039
Mary Geddes <P12197502@GH582-003.lec-admin.dmu.ac.uk>
2014-01-08 10:27:42 +0000

    introduced a third line

--- hello.c
+++ hello.c
@@ -2,6 +2,8 @@
 
 int main()
 {
-        printf("Hello World\n");
+        printf("Hello World!\n");
+        printf("Fare thee Well!\n");
+        printf("Quicker version\n");
         return 0;
 }
