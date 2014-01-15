7fc090420f8a62faa32b8082de8d52459e87a7cf
Mary Geddes <P12197502@GH582-003.lec-admin.dmu.ac.uk>
2014-01-15 10:20:52 +0000

    functionality to subtract added

--- maths.c
+++ maths.c
@@ -4,6 +4,10 @@ int main()
         int x, y;
         printf("Enter two values: ");
         scanf(" %i\n %i\n", &x, &y);
-        printf(" %i + %i = %i\n", x, y, x+y);
+        printf("%i + %i = %i\n", x, y, x+y);
+        if (x > y)
+          printf("%i - %i = %i\n", x, y, x-y);
+        else
+          printf("%i - %i = %i\n", y, x, y-x);
         return 0;
 }
