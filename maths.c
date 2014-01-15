cf0aa9cba2f32fa8dbf55b096b451b64464c696e
Mary Geddes <P12197502@GH582-003.lec-admin.dmu.ac.uk>
2014-01-15 10:27:21 +0000

    function to multiply and divide added

--- maths.c
+++ maths.c
@@ -2,12 +2,15 @@
 int main()
 {
         int x, y;
-        printf("Enter two values: ");
-        scanf(" %i\n %i\n", &x, &y);
+        printf("Enter two values:\n");
+        scanf("%i %i\n", &x, &y);
         printf("%i + %i = %i\n", x, y, x+y);
-        if (x > y)
+        if (x > y){
           printf("%i - %i = %i\n", x, y, x-y);
-        else
+          printf("%i / %i = %i\n", x, y, x/y); }
+        else{
           printf("%i - %i = %i\n", y, x, y-x);
+          printf("%i / %i = %i\n", y, x, y/x);}
+        printf("%i * %i = %i\n", x, y, x*y);
         return 0;
 }
