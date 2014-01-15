59aa5024dc8e77d0ae0e159022a912bbbcdd4d79
Mary Geddes <P12197502@GH582-003.lec-admin.dmu.ac.uk>
2014-01-15 10:11:40 +0000

    function to add introduced

---
+++ maths.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+        int x, y;
+        printf("Enter two values: ");
+        scanf(" %i\n %i\n", &x, &y);
+        printf(" %i + %i = %i\n", x, y, x+y);
+        return 0;
+}
